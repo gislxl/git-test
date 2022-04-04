4d87e804a151d7b4cf89287219b347b55af52276
lxl <lxl@scau.com>
2022-04-04 11:24:41 +0800

    third commit

--- src/main/java/com/gis/GitTest.java
+++ src/main/java/com/gis/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("1111111111111111111");
+        System.out.println("2222222222222222");
     }
 }
